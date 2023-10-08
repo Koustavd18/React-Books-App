e39999f35509e96b2b91d03e675acb8af93e7e1a
Koustav Das <koustavdas@Koustavs-Mac.local>
2023-10-08 12:35:19 +0530

    updates

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -18,7 +18,8 @@ function BookCreate({ onCreate }) {
     return (
         <div className='book-create'>
             <form onSubmit={handleSubmit}>
-                <label>Title</label>
+                <h1>Add a Book</h1>
+                <label htmlFor='input' style={{fontSize : 1.5 +'rem'}}>Title </label>
                 <input className='input' value={title} onChange={handleChange} />
                 <button className='button'>Create Book</button>
             </form>
