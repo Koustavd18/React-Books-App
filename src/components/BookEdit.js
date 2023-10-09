afda1137bd3d7c189dc6efa6e6104419a841cd58
Koustav Das <koustavdas@Koustavs-Mac.local>
2023-10-09 21:12:55 +0530

    updates

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,6 +1,24 @@
-function BookEdit() {
+import { useState } from 'react';
+
+function BookEdit({book}) {
+
+    const [title, setTitle] = useState(book.title);
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`the new title is ${title}`);
+    }
+
     return (
-        <div>BooksEdit</div>
+        <form className='book-edit' onSubmit={handleSubmit}>
+            <label htmlFor='title'>Title</label>
+            <input className='input' name='title' type='text' value={title} onChange={handleTitleChange} />
+            <button className='button is-primary' >Save</button>
+        </form>
     );
 }
 
